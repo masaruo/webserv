47146b99dcf0d549b35acdda661e9eb25f2732ae
masaru ogawa <masaruo@gmail.com>
2024-08-10 02:05:53 +0000

    RequestHeader class initial completed. no error handling and passed initial test

--- include/request/RequestHeader.hpp
+++ include/request/RequestHeader.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <map>
 #include <string>
+#include "string.hpp"
 
 class RequestHeader
 {
@@ -10,10 +11,15 @@ public:
 	typedef header_map::iterator				iterator;
 private:
 	header_map	headers_;
-public:
+	void	make_headers(ft::string::string_vector const &str_vec);
 	RequestHeader();
+public:
+	RequestHeader(ft::string::string_vector const &str_vec);
 	~RequestHeader();
 	RequestHeader(RequestHeader const &rhs);
 	RequestHeader &operator=(RequestHeader const &rhs);
-
+	iterator		get_pair(std::string const &key);
+	const_iterator	get_pair(std::string const &key) const;
+	//todo error
+	//todo duplicate key
 };
--- src/request/RequestHeader.cpp
+++ src/request/RequestHeader.cpp
@@ -1,1 +1,60 @@
 #include "RequestHeader.hpp"
+#include <utility>
+
+RequestHeader::RequestHeader(ft::string::string_vector const &str_vec)
+:headers_()
+{
+	make_headers(str_vec);
+	return ;
+}
+
+RequestHeader::~RequestHeader()
+{
+	return ;
+}
+
+RequestHeader::RequestHeader(RequestHeader const &rhs)
+:headers_(rhs.headers_)
+{
+	return ;
+}
+
+RequestHeader &RequestHeader::operator=(RequestHeader const &rhs)
+{
+	if (this != &rhs)
+	{
+		headers_ = rhs.headers_;
+	}
+	return (*this);
+}
+
+void	RequestHeader::make_headers(ft::string::string_vector const &str_vec)
+{
+	ft::string::string_vector_const_iterator iter = str_vec.begin();
+	ft::string::string_vector_const_iterator end = str_vec.end();
+
+	while (iter != end)
+	{
+		ft::string::string_vector	split_by_doubleColon = iter->split(":");
+		//todo ERROR and verfication
+		split_by_doubleColon.at(1).trim(ft::string::SP);
+		headers_.insert(std::make_pair(split_by_doubleColon.at(0), split_by_doubleColon.at(1)));
+		iter++;
+	}
+}
+
+RequestHeader::const_iterator RequestHeader::get_pair(std::string const &key) const
+{
+	RequestHeader::const_iterator	found;
+
+	found = headers_.find(key);
+	return (found);
+}
+
+RequestHeader::iterator	RequestHeader::get_pair(std::string const &key)
+{
+	RequestHeader::iterator	found;
+
+	found = headers_.find(key);
+	return (found);
+}

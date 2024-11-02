02849c7452fd8837369b847896f5794ce899738f
resaito <resaito@student.42tokyo.jp>
2024-11-02 16:10:42 +0900

    getAcceptedPorts 実装

--- src/config/ConfigFactory.cpp
+++ src/config/ConfigFactory.cpp
@@ -74,32 +74,45 @@ config::Config	config::ConfigFactory::getConfig(std::string const &server_name) 
 	return (configs_.front());
 }
 
+std::vector<std::size_t> config::ConfigFactory::getAcceptedPorts(void) const
+{
+	std::vector<size_t> ports;
+	for(size_t i = 0; i < configs_.size(); i++)
+		ports.push_back(configs_[i].getPort());
+
+	return (ports);
+}
+
 // int main()
 // {
 // 	try
 // 	{
 // 		config::ConfigFactory a("simple.conf");
-// 		std::cout << a.getConfig("localhost").getServerName() << std::endl;
-// 		std::cout << a.getConfig("2").getServerName() << std::endl;
-// 		std::cout << std::endl;
+// 		// std::cout << a.getConfig("localhost").getServerName() << std::endl;
+// 		// std::cout << a.getConfig("2").getServerName() << std::endl;
+// 		// std::cout << std::endl;
 
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/").pathType_ << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::INDEX) << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::ALLOWED_METHOD) << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getLastValue(config::Config::ALLOWED_METHOD) << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::AUTOINDEX) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/").pathType_ << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::INDEX) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::ALLOWED_METHOD) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getLastValue(config::Config::ALLOWED_METHOD) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/").directive_.getFirstValue(config::Config::AUTOINDEX) << std::endl;
 		
-// 		std::cout << std::endl;
+// 		// std::cout << std::endl;
 
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/uploads").pathType_ << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/uploads").directive_.getFirstValue(config::Config::ALLOWED_METHOD) << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/uploads").directive_.getFirstValue(config::Config::UPLOAD_ROOT) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/uploads").pathType_ << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/uploads").directive_.getFirstValue(config::Config::ALLOWED_METHOD) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/uploads").directive_.getFirstValue(config::Config::UPLOAD_ROOT) << std::endl;
 		
-// 		std::cout << std::endl;
+// 		// std::cout << std::endl;
 		
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/redirect").pathType_ << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/redirect").directive_.getFirstValue(config::Config::REDIRECT_TO) << std::endl;
-// 		std::cout << a.getConfig("localhost").getConfigLocation("/redirect").directive_.getLastValue(config::Config::REDIRECT_TO) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/redirect").pathType_ << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/redirect").directive_.getFirstValue(config::Config::REDIRECT_TO) << std::endl;
+// 		// std::cout << a.getConfig("localhost").getConfigLocation("/redirect").directive_.getLastValue(config::Config::REDIRECT_TO) << std::endl;
+
+// 		std::vector<std::size_t> ports = a.getAcceptedPorts();
+// 		for (int i = 0; i < ports.size(); i++)
+// 			std::cout << ports[i] << std::endl;
 // 	}
 // 	catch(const std::exception& e)
 // 	{

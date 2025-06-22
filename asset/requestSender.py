df3ec24f57dc1db4478f814d699139984ed1fc8e
masaru ogawa <masaruo@gmail.com>
2025-06-22 12:32:31 +0900

    requestSender.py updated and brief README.md added

--- asset/requestSender.py
+++ asset/requestSender.py
@@ -4,7 +4,7 @@ import socket
 def send_chunked():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('localhost', 8888))
-    
+
     request = (
         b'POST /cgi-bin/echo.py/path/info HTTP/1.1\r\n'
         b'Host: localhost:8888\r\n'
@@ -12,7 +12,7 @@ def send_chunked():
         b'Transfer-Encoding: chunked\r\n'
         b'\r\n'
         b'A\r\n'
-        b'name=masar\r\n'
+        b'name=xxxxx\r\n'
         b'A\r\n'
         b'u&message=\r\n'
         b'B\r\n'
@@ -21,7 +21,7 @@ def send_chunked():
         b'm_post\r\n'
         b'0\r\n\r\n'
     )
-    
+
     sock.send(request)
     response = sock.recv(4096)
     print(response.decode())
